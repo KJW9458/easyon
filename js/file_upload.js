0411876b82a5cde403a9d9acbfe156050a2cdf4f
KJW9458 <69436349+KJW9458@users.noreply.github.com>
2021-01-05 10:41:54 +0900

    Add files via upload

---
+++ js/file_upload.js
@@ -0,0 +1,30 @@
+function change(obj) {
+    var str = obj.value;
+    var num = str.length;
+    var i = str.lastIndexOf("\\");
+    var index = str.substr(i + 1, num);
+    document.getElementById('file_name').innerHTML = index;
+    // document.querySelector('#sendFile').click();
+
+    var form = $('#fileInput')[0];
+
+    var formData = new FormData();
+    formData.append('file', $('#fileInput')[0].files[0]);
+
+    // $.ajax({
+    //     url: 'upload.php',
+    //     dataType: 'text',
+    //     cache: false,
+    //     contentType: false,
+    //     processData: false,
+    //     data: formData,
+    //     type: 'POST',
+    //     success: function(response) {
+    //         console.log(response);
+    //         $('#read').load('readdir.php');
+    //     },
+    //     error: function(error) {
+    //         console.log(error);
+    //     }
+    // });
+}
--- js/search.js
+++ js/search.js
@@ -12,19 +12,19 @@
             cache: false,
             success: function(data) {
                 console.log(data);
-                // var n = JSON.parse(data);
-                // console.log(n.items[0].title);
-                // var end = n.display;
-                // var content = "";
-                // $('#naver').html('');
-                // for (var i = n.start; i <= end; i++) {
-                //     content += "<div>";
-                //     content += "<div><img src='" + n.items[i].thumbnail + "' style='width:" + n.items[i].sizewidth + ";height:" + n.items[i].sizewidth + "'></div>";
-                //     content += "<div>" + n.items[i].title + "</div>";
-                //     //                    content += "<div>"+n.items[i].description+"</div>";
-                //     content += "</div><br>";
-                //     $('#naver').html(content);
-                // }
+                var n = JSON.parse(data);
+                console.log(n.items[0].title);
+                var end = n.display;
+                var content = "";
+                $('#naver').html('');
+                for (var i = n.start; i <= end; i++) {
+                    content += "<div>";
+                    content += "<div><img src='" + n.items[i].thumbnail + "' style='width:" + n.items[i].sizewidth + ";height:" + n.items[i].sizewidth + "'></div>";
+                    content += "<div>" + n.items[i].title + "</div>";
+                    //                    content += "<div>"+n.items[i].description+"</div>";
+                    content += "</div><br>";
+                    $('#naver').html(content);
+                }
             },
             error: function(e) {
                 console.log(e);
