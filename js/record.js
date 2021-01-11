d5de8e72edfab3b5af3ab1c0a9eca1c871b71e06
zozowlsdn <zozowlsdn@naver.com>
2021-01-11 15:59:29 +0900

    test

---
+++ js/record.js
@@ -0,0 +1,21 @@
+var time = 1200;
+var min = "";
+var sec = "";
+
+////    var x = setInterval(function() {
+////        min = parseInt(time/60);
+////        if((time%60)<10){
+////            sec = "0"+time%60;
+////        } else {
+////            sec = time%60;
+////        }
+////        
+//
+//        document.getElementById('time').innerHTML = min + " : " + sec;
+//        time--;
+//
+//        if(time<0){
+//            clearInterval(x);
+//            document.getElementById('time').innerHTML = "00:00";
+//        }
+//    }, 1000);
