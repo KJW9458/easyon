f95178fa8392c057a065eed17a866eb632448e73
zozowlsdn <zozowlsdn@naver.com>
2021-01-14 17:24:35 +0900

    test

--- js/room.js
+++ js/room.js
@@ -202,3 +202,20 @@ enterBtn.addEventListener("click",
 // $('#stopCall').click(function() {
 //     remonCall.close();
 // });
+
+navigator.mediaDevices.getUserMedia({
+    audio: true
+}).then(function(audioStream){
+    //오디오 스트림을 얻어냄
+    navigator.mediaDevices.getDisplayMedia({
+        audio: true,
+        video: true
+    }).then(function(screenStream){
+        //스크린 공유 스트림을 얻어내고 여기에 오디오 스트림을 결합함
+        screenStream.addTrack(audioStream.getAudioTracks()[0]);
+    }).catch(function(e){
+        // error;
+    })
+}).catch(function(e){
+    // error;
+});
